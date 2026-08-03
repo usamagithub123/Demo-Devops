07f31f76a13eeb42ec711faae0eebd2c74af9337
usamagithub123 <81920730+usamagithub123@users.noreply.github.com>
2026-08-03 19:00:42 +0000

    app.py is now is flask

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask
 
+app = Flask(__name__)
+   # <-- enables /metrics
+
+@app.route("/")
+def hello():
+    return "Hello World!"                   
+
+@app.route("/new")
+def new():
+    return "FINAL CHECK"            
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
